a6405324e71200995de05c68baf6c78d6a0cac46
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-07-27 02:19:09 -0500

    componentes de la aplicacion con migración completa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container,Row } from 'react-bootstrap';
+import Head from './components/Head'
+import CntJumbotron from './components/CntJumbotron'
+import CntBox1 from './components/CntBox1'
+import CntBox2 from './components/CntBox2'
+import CntBox3 from './components/CntBox3'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Autor: Oscar Mesa
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Head/>
+        <CntJumbotron/>
+        <Row>
+          <CntBox1/><CntBox2/><CntBox3/>
+        </Row>
+    </Container>
   );
 }
 
