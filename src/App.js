633baab4825db2fdf452099b7e34509e7e1ffba0
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-07-26 22:45:49 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Autor: Oscar Mesa
         </a>
       </header>
     </div>
